5f554cdc90d56f101664dd8116ae53f84f19e346
William Yeung <williamyeung@Williams-Mac-mini.local>
2021-02-08 17:14:26 -0500

    First commit

---
+++ GoodEats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodEats
+//
+//  Created by William Yeung on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
